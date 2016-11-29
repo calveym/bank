47402f05469b5abb782232d4bfcef65b664e15c5
LMCMLJ <mjcalvey2@gmail.com>
2016-11-29 13:20:06 +0000

    Improves formatting of print. Several big print bugs remaining

---
+++ README.md
@@ -0,0 +1,6 @@
+## Bank tech test
+This is my response to the [following tech test.](https://github.com/makersacademy/course/blob/master/individual_challenges/bank_tech_test.md)
+I wrote my solution in javascript, and it runs locally in the browser.
+
+###Architecture:
+The solution is based around two main classes: BankAccount, which handles all aspects of dealing with the account, depositing withdrawing etc, and AccountStatement, which deals with recording bank history and formatting the history for pretty printing.
--- index.js
+++ index.js
@@ -2,19 +2,19 @@ window.onload = function () {
 
   BankAccount = function () {
     this.balance = 0;
-    this.accountStatement = new AccountStatement();
+    this.as = new AccountStatement();
   };
 
   BankAccount.prototype = {
 
     deposit: function (time, amount) {
       this.balance += amount;
-      this.accountStatement.storeNewEntry(time, amount, "credit");
+      this.as.storeNewEntry(time, amount, "credit", this.balance);
     },
 
     withdraw: function (time, amount) {
       this.balance -= amount;
-      this.accountStatement.storeNewEntry(time, amount, "debit", this.balance);
+      this.as.storeNewEntry(time, amount, "debit", this.balance);
     }
   };
 
@@ -26,25 +26,62 @@ window.onload = function () {
   AccountStatement.prototype = {
 
     print: function () {
+      console.log("date       || credit || debit  || balance");
       for(i = 0; i < this.history.length; i++) {
-        console.log("date     || credit || debit  || balance");
-        console.log(this.history[i].date + "|| " + this.history[i].credit + "|| " + this.history[i].debit + "||" + this.history[i].balance);
+        console.log(this.history[i].time + "|| " + this.history[i].credit + "|| " + this.history[i].debit + "|| " + this.history[i].balance);
       }
     },
 
-    storeNewEntry: function (time, amount, type, balance) {
-      var tempEntry = {
-        time: time,
-        credit: "",
-        debit: "",
-        balance: balance
-      };
-      if (type === "debit") {
-        tempEntry.debit = amount.toString();
+    storeNewEntry: function (time, amount, type, bal) {
+      var tempEntry = {};
+      console.log(this.formatCredit(type, 2000, 10));
+      tempEntry.time = this.formatTime(time, 11);
+      tempEntry.credit = this.formatCredit(type, amount, 10);
+      tempEntry.debit = this.formatDebit(type, amount, 10);
+      tempEntry.balance = bal;
+      this.history.unshift(tempEntry);
+    },
+
+    formatTime: function (time, len) {
+      // ASK MARY ABOUT WHY DIRECTLY USING THE VARS DIDNT WORK
+      var repeatAmount = len - time.length;
+      return (time.toString() + " ".repeat(repeatAmount));
+    },
+
+    formatCredit: function (type, amount, len) {
+      var repeatAmount = len - amount.length;
+      if(type === "credit") {
+        return (amount.toString() + "x".repeat(repeatAmount));
       } else {
-        tempEntry.credit = amount.toString();
+        return (" ".repeat(len));
       }
-      this.history.push(tempEntry);
+    },
+
+    formatDebit: function (type, amount, len) {
+      var repeatAmount = len - amount.length;
+      if(type === "debit") {
+        return (amount.toString() + " ".repeat(repeatAmount));
+      } else {
+        return (" " * len);
+      }
     }
   };
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("temp");
